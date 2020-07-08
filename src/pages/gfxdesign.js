9c71a0940df74f50634ea638f17f95568e3d303a
masayaShinoda <masayashida@gmail.com>
2020-07-08 18:01:44 +0700

    fix attemtp

--- src/pages/gfxdesign.js
+++ src/pages/gfxdesign.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import GfxDesign from "../components/GfxDesign"
+import GfxDesign from "../components/gfxdesign"
 
 import "../components/styles.css"
 
