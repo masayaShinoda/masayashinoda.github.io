d0aa7f501b976ff92bfde0e1c953550a5d5b17b9
masayaShinoda <masayashida@gmail.com>
2020-07-08 17:22:56 +0700

    gfx design portfolio commit

--- src/components/gfxdesign.js
+++ src/components/gfxdesign.js
@@ -7,12 +7,7 @@ import SEO from "./seo"
 import "./styles.css"
 
 //images assets import
-import wisJournalDesktop from "../images/wis-journal-desktop.jpg"
-import wisJournalMobile from "../images/wis-journal-mobile.jpg"
-import masayaBlogDesktop from "../images/masayablog-desktop.png"
-import masayaBlogMobile from "../images/masayablog-mobile.png"
-import masayaPhotoDesktop from "../images/masayaphoto-desktop.jpg"
-import masayaPhotoMobile from "../images/masayaphoto-mobile.jpg"
+import baristaHiring from "../images/hiring-barista_widescreen.jpg"
 
 const WebDev = () => {
   return (
@@ -20,30 +15,11 @@ const WebDev = () => {
       <div className="item-container">
         <h2>Yomie's Rice X Yogurt Cambodia</h2>
         <p>
-          Digital and print design work for local yogurt shop in Phnom Penh,
-          Cambodia.
+          Digital and print design work for local yogurt and drinks shop in
+          Phnom Penh, Cambodia.
         </p>
-        <h3>Wokrs include:</h3>
-        <ul>
-          {/* <li>Social media content</li>
-          <li>I-stand banner</li>
-          <li>Menu</li>
-          <li>Business card</li> */}
-        </ul>
-        <Link to="https://wis-journal.com" style={{ margin: `0` }}>
-          <img
-            className="showcaseDesktop"
-            src={wisJournalDesktop}
-            alt="wis-journal.com portfolio item showcase"
-          ></img>
-        </Link>
-        <Link to="https://wis-journal.com" style={{ margin: `0` }}>
-          <img
-            className="showcaseMobile"
-            src={wisJournalMobile}
-            alt="wis-journal.com portfolio item showcase"
-          ></img>
-        </Link>
+        <h3>Works include:</h3>
+        <img src={baristaHiring}></img>
       </div>
     </>
   )
